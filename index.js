a2b9db1ca0e90b32db77c277e92d8ddbb3a7d8b5
Alelsito <lbalelxander@gmail.com>
2022-07-27 00:10:45 -0500

    Search by name & id

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 // SELECTORs
 const containerCards = document.querySelector("#containerCards");
+const form = document.querySelector("#searcher");
 
 const marvelData = {
   render: () => {
@@ -11,27 +12,68 @@ const marvelData = {
 
     fetch(marvelAPI)
       .then((respuesta) => respuesta.json())
-      .then((data) => renderCards(data));
+      .then((data) => renderCards(data.data.results));
   },
 };
 
 marvelData.render();
 
 const renderCards = (characters) => {
-    // console.log(characters);
-    characters.data.results.forEach((character) => {
-        console.log(character);
-        containerCards.innerHTML +=
-        `<div class="col-2 card mb-3">
-            <div class="title-card">
-                <span class="title">${character.name}</span>
-            </div>
-            <img class="image" src="${character.thumbnail.path}.${character.thumbnail.extension}" alt="${character.name}">
-            <div class="image-degradado"></div>
-            <div class="bar">
-                <div class="emptybar"></div>
-                <div class="filledbar"></div>
-            </div>
-        </div>` 
+  containerCards.innerHTML = "";
+  if (characters.size !== 0) {
+    characters.forEach((character) => {
+      containerCards.innerHTML += `<div class="col-2 card mb-3">
+              <div class="title-card">
+                  <span class="title">${character.name}</span>
+              </div>
+              <img class="image" src="${character.thumbnail.path}.${character.thumbnail.extension}" alt="${character.name}">
+              <div class="image-degradado"></div>
+              <div class="bar">
+                  <div class="emptybar"></div>
+                  <div class="filledbar"></div>
+              </div>
+          </div>`;
+    });
+} else {
+      console.log("No hay resulados")
+  }
+};
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  search(evt.target.character.value);
+});
+
+const search = (characterSearch) => {
+  const marvelAPI =
+    "http://gateway.marvel.com/v1/public/characters?ts=2022&apikey=4af096836a1b7a7c3b2658feeec3ae3a&hash=0725b34b13436807e63352ce699e265f";
+
+  fetch(marvelAPI)
+    .then((respuesta) => respuesta.json())
+    .then((data) => {
+      let characterName = filterByName(data, characterSearch);
+      let characterId = filterById(data, characterSearch);
+
+      let unique = new Set([...characterName, ...characterId]);
+      renderCards(unique);
+      console.log(unique)
+    });
+};
+
+const filterByName = (data, characterSearch) => {
+    let array = data.data.results;
+    let name = array.filter((character) => {
+        return character.name.toLowerCase().includes(characterSearch.toLowerCase());
     })
-}
+    return name;
+};
+
+const filterById = (data, characterSearch) => {
+    let array = data.data.results;
+    let name = array.filter((character) => {
+        let id = String(character.id);
+        return id.includes(characterSearch);
+    })
+    return name;
+};
+
