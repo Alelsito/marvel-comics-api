84697fced35377aac9f12f719afbffe5bd68d7d4
Alelsito <lbalelxander@gmail.com>
2022-07-27 22:34:15 -0500

    Specific character eventListenerSearch

--- index.js
+++ index.js
@@ -4,6 +4,15 @@
 // SELECTORs
 const containerCards = document.querySelector("#containerCards");
 const form = document.querySelector("#searcher");
+const characterInformation = document.querySelector("#characterInformation");
+const containerCharacter = document.querySelector(".container-character");
+const containerSeriesComicsCards = document.querySelector(".container-series-comics-cards");
+const comicsTitle = document.querySelector("#comicsTitle");
+const comicsCards = document.querySelector("#comics");
+const seriesTitle = document.querySelector("#seriesTitle");
+const seriesCards = document.querySelector("#series");
+
+// containerSeriesComicsCards.classList.add("container-visibility")
 
 // API DATA FOR RENDER
 const marvelData = {
@@ -21,9 +30,11 @@ marvelData.render();
 
 // RENDER CARDS
 const renderCards = (characters) => {
-  // containerCards.innerHTML = "";
+  // console.log(characters)
+  containerCards.innerHTML = "";
   if (characters.size !== 0) {
     characters.forEach((character) => {
+      // console.log(character)
       let card = document.createElement("div");
       let titleCard = document.createElement("div");
       let title = document.createElement("span");
@@ -82,27 +93,76 @@ const renderCards = (characters) => {
 // EVENT LISTENER SEARCH
 
 const eventListenerSearch = (e) => {
-  console.log(e);
-  const marvelComicsAPI =
-    `http://gateway.marvel.com/v1/public/characters/${e}/comics?ts=2022&apikey=4af096836a1b7a7c3b2658feeec3ae3a&hash=0725b34b13436807e63352ce699e265f`;
+  const marvelAPI = `http://gateway.marvel.com/v1/public/characters/${e}?ts=2022&apikey=4af096836a1b7a7c3b2658feeec3ae3a&hash=0725b34b13436807e63352ce699e265f`;
 
-  const marvelSeriesAPI =
-    `http://gateway.marvel.com/v1/public/characters/${e}/series?ts=2022&apikey=4af096836a1b7a7c3b2658feeec3ae3a&hash=0725b34b13436807e63352ce699e265f`;
+  const marvelComicsAPI = `http://gateway.marvel.com/v1/public/characters/${e}/comics?ts=2022&apikey=4af096836a1b7a7c3b2658feeec3ae3a&hash=0725b34b13436807e63352ce699e265f`;
+
+  const marvelSeriesAPI = `http://gateway.marvel.com/v1/public/characters/${e}/series?ts=2022&apikey=4af096836a1b7a7c3b2658feeec3ae3a&hash=0725b34b13436807e63352ce699e265f`;
+
+  fetch(marvelAPI)
+    .then((respuesta) => respuesta.json())
+    .then((data) => renderCharacterInformation(data.data.results[0]));
 
   fetch(marvelComicsAPI)
     .then((respuesta) => respuesta.json())
-    .then((data) => console.log(data))
+    .then((data) => renderCharacterComics(data.data.results));
 
   fetch(marvelSeriesAPI)
     .then((respuesta) => respuesta.json())
-    .then((data) => console.log(data))
-
-
+    .then((data) => renderCharacterSeries(data.data.results))
 };
 
-const renderCharacerInformation = (character) => {
-  console.log(character)
+const renderCharacterInformation = (character) => {
+  containerCharacter.classList.add("container-visibility");
   containerCards.innerHTML = "";
+
+  characterInformation.innerHTML += `            
+  <div class="col d-flex justify-content-end">
+    <img class="specific-character-image"
+      src="${character.thumbnail.path}.${character.thumbnail.extension}" alt="name">
+  </div>
+  <div class="col column-main">
+    <div class="col-6">
+      <span class="fs-1 specific-character-title">${character.name}</span>
+    </div>
+    <div class="col-5">
+      <span class="specific-character-description">${character.description}</span>
+    </div>
+  </div>`;
+};
+
+const renderCharacterComics = (character) => {
+  comicsTitle.innerHTML += `
+  <div class="col-12 principal d-flex justify-content-center">
+    <span class="fs-3 principal-title">COMICS</span>
+  </div>
+  <div class="col-12 division">
+  </div>`;
+
+  character.forEach((comic) => {
+    comicsCards.innerHTML += `
+    <div class="col-2 cards">
+      <img class="cards-image" src="${comic.thumbnail.path}.${comic.thumbnail.extension}" alt="">
+      <span class="cards-title">${comic.title}</span>
+    </div>`;
+  });
+};
+
+const renderCharacterSeries = (character) => {
+  seriesTitle.innerHTML +=`
+  <div class="col-12 principal d-flex justify-content-center">
+    <span class="fs-3 principal-title">SERIES</span>
+  </div>
+  <div class="col-12 division">
+  </div>`
+
+  character.forEach((serie) => {
+    seriesCards.innerHTML += `
+    <div class="col-2 cards">
+      <img class="cards-image" src="${serie.thumbnail.path}.${serie.thumbnail.extension}" alt="">
+      <span class="cards-title">${serie.title}</span>
+    </div>`
+  })
 }
 
 // SEARCH
